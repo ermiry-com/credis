1748020bb8c42c488342b73dc8b353e25c593e41
ermiry <erick.salas@ermiry.com>
2021-06-05 06:01:34 -0500

    Version 0.1

--- include/credis/version.h
+++ include/credis/version.h
@@ -1,11 +1,15 @@
 #ifndef _CREDIS_VERSION_H_
 #define _CREDIS_VERSION_H_
 
-#define CREDIS_VERSION						"1.0"
-#define CREDIS_VERSION_NAME					"Release 1.0"
-#define CREDIS_VERSION_DATE					"04/12/2020"
-#define CREDIS_VERSION_TIME					"19:38 CST"
-#define CREDIS_VERSION_AUTHOR				"Erick Salas"
+#define CREDIS_VERSION				"0.1"
+#define CREDIS_VERSION_NAME			"Version 0.1"
+#define CREDIS_VERSION_DATE			"05/06/2021"
+#define CREDIS_VERSION_TIME			"06:01 CST"
+#define CREDIS_VERSION_AUTHOR		"Erick Salas"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 // print full credis version information
 extern void credis_version_print_full (void);
@@ -15,5 +19,9 @@ extern void credis_version_print_version_id (void);
 
 // print the version name
 extern void credis_version_print_version_name (void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- src/credis/version.c
+++ src/credis/version.c
@@ -2,25 +2,25 @@
 
 #include "credis/version.h"
 
-// print full erver version information
+// print full credis version information
 void credis_version_print_full (void) {
 
-	printf ("\nCredis Version: %s\n", CREDIS_VERSION_NAME);
-	printf ("Release Date & time: %s - %s\n", CREDIS_VERSION_DATE, CREDIS_VERSION_TIME);
-	printf ("Author: %s\n\n", CREDIS_VERSION_AUTHOR);
+	(void) printf ("\nCredis Version: %s\n", CREDIS_VERSION_NAME);
+	(void) printf ("Release Date & time: %s - %s\n", CREDIS_VERSION_DATE, CREDIS_VERSION_TIME);
+	(void) printf ("Author: %s\n\n", CREDIS_VERSION_AUTHOR);
 
 }
 
 // print the version id
 void credis_version_print_version_id (void) {
 
-	printf ("\nCredis Version ID: %s\n", CREDIS_VERSION);
+	(void) printf ("\nCredis Version ID: %s\n", CREDIS_VERSION);
 
 }
 
 // print the version name
 void credis_version_print_version_name (void) {
 
-	printf ("\nCredis Version: %s\n", CREDIS_VERSION_NAME);
+	(void) printf ("\nCredis Version: %s\n", CREDIS_VERSION_NAME);
 
 }
