8bee3b23a18fd4a33e90a5b89b6681cc3a31ec7c
ermiry <erick.salas@ermiry.com>
2021-06-05 05:15:47 -0500

    Added dedicated config header

---
+++ include/credis/config.h
@@ -0,0 +1,13 @@
+#ifndef _CREDIS_CONFIG_H_
+#define _CREDIS_CONFIG_H_
+
+// to be used in the application
+#define CREDIS_EXPORT 			extern
+
+// to be used by internal credis methods
+#define CREDIS_PRIVATE			extern
+
+// used by private methods but can be used by the application
+#define CREDIS_PUBLIC 			extern
+
+#endif
