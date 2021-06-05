765b1d20f6f10ec13bdfdb97ee1d06838ace1cfa
ermiry <erick.salas@ermiry.com>
2021-06-05 02:22:02 -0500

    Updated existing main methods

--- include/credis/redis.h
+++ include/credis/redis.h
@@ -1,9 +1,7 @@
 #ifndef _REDIS_H_
 #define _REDIS_H_
 
-#include "types/string.h"
-
-#pragma region status
+#define REDIS_HOST_SIZE				128
 
 typedef enum RedisStatus {
 
@@ -14,21 +12,15 @@ typedef enum RedisStatus {
 
 extern RedisStatus redis_get_status (void);
 
-#pragma endregion
-
-#pragma region values
-
 extern void redis_set_hostname (const char *host);
 
-extern void redis_set_port (int p);
+extern void redis_set_port (const unsigned int port);
 
 // sets the connection timeout
 // default to 1.5 seconds
-extern void redis_set_timeout (long seconds, long miliseconds);
-
-#pragma endregion
-
-#pragma region connection
+extern void redis_set_timeout (
+	const long seconds, const long miliseconds
+);
 
 // connects to the redis server
 // returns 0 on success, 1 on error
@@ -41,19 +33,5 @@ extern unsigned int redis_disconnect (void);
 // pings the db to test for a success connection
 // returns 0 on success, 1 on error
 extern unsigned int redis_ping_db (void);
-
-#pragma endregion
-
-#pragma region CRUD
-
-// sets a new simple key value
-// returns 0 on success, 1 on error
-extern unsigned int redis_set_key (const char *key, const char *value);
-
-// gets a simple value for a key
-// returns a newly allocated string that should be free
-extern String *redis_get_value (const char *key);
-
-#pragma endregion
 
 #endif
--- src/credis/redis.c
+++ src/credis/redis.c
@@ -2,92 +2,45 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <cerver/utils/log.h>
+
 #include <hiredis/hiredis.h>
 
-#include <clibs/types/string.h>
-
-#include "credis/redis.h"
-
-#pragma region utils
-
-// creates a new c string with the desired format, as in printf
-static char *redis_c_string_create (const char *format, ...) {
-
-	char *fmt = NULL;
-
-	if (format != NULL) fmt = strdup (format);
-	else fmt = strdup ("");
-
-	va_list argp;
-	va_start (argp, format);
-	char oneChar[1] = { 0 };
-	int len = vsnprintf (oneChar, 1, fmt, argp);
-	if (len < 1) return NULL;
-	va_end (argp);
-
-	char *str = (char *) calloc (len + 1, sizeof (char));
-	if (!str) return NULL;
-
-	va_start (argp, format);
-	vsnprintf (str, len + 1, fmt, argp);
-	va_end (argp);
-
-	free (fmt);
-
-	return str;
-
-}
-
-// prints a red error message to stderr
-static void redis_log_error (const char *msg) {
-
-	if (msg) fprintf (stderr, "\x1b[31m" "[REDIS][ERROR]: " "%s\n" "\x1b[0m", msg);
-
-}
-
-#pragma endregion
-
-#pragma region status
-
-static RedisStatus status = REDIS_STATUS_DISCONNECTED;
-
-RedisStatus redis_get_status (void) { return status; }
-
-#pragma endregion
-
-#pragma region values
+#include "redis.h"
 
 static redisContext *redis_context = NULL;
 
-static String *hostname = NULL;
+static RedisStatus status = REDIS_STATUS_DISCONNECTED;
+
+static unsigned int redis_port = 6379;
+static char redis_host[REDIS_HOST_SIZE] = { 0 };
+
+static struct timeval timeout = { 1, 500000 };
+
+RedisStatus redis_get_status (void) { return status; }
 
 void redis_set_hostname (const char *host) {
 
 	if (host) {
-		str_delete (hostname);
-		hostname = host ? str_new (host) : NULL;
+		(void) strncpy (redis_host, host, REDIS_HOST_SIZE - 1);
 	}
 
 }
 
-static int port = 0;
+void redis_set_port (const unsigned int port) {
 
-void redis_set_port (int p) { port = p; }
+	redis_port = port;
 
-static struct timeval timeout = { 1, 500000 };
+}
 
 // sets the connection timeout
 // default to 1.5 seconds
-void redis_set_timeout (long seconds, long miliseconds) {
+void redis_set_timeout (const long seconds, const long miliseconds) {
 
 	timeout.tv_sec = seconds;
 	timeout.tv_usec = miliseconds * 1000;
 
 }
-
-#pragma endregion
-
-#pragma region connection
 
 // connects to the redis server
 // returns 0 on success, 1 on error
@@ -96,19 +49,17 @@ unsigned int redis_connect (void) {
 	unsigned int retval = 1;
 
 	if (status == REDIS_STATUS_DISCONNECTED) {
-		redis_context = redisConnectWithTimeout (
-			hostname->str, 
-			port,
-			timeout
-		);
+		redisOptions o = { 0 };
+		REDIS_OPTIONS_SET_TCP (&o, redis_host, redis_port);
+
+		redis_context = redisConnectWithOptions (&o);
 
 		if (redis_context) {
 			if (redis_context->err) {
-				char *s = redis_c_string_create ("Connection error: %s\n", redis_context->errstr);
-				if (s) {
-					redis_log_error (s);
-					free (s);
-				}
+				cerver_log_error (
+					"REDIS - connection error: %s",
+					redis_context->errstr
+				);
 
 				redisFree (redis_context);
 			}
@@ -121,12 +72,12 @@ unsigned int redis_connect (void) {
 		}
 
 		else {
-			redis_log_error ("Failed to connect to redis!");
+			cerver_log_error ("Failed to connect to redis!");
 		}
 	}
 
 	else {
-		redis_log_error ("Already connected to redis!");
+		cerver_log_warning ("Already connected to redis!");
 	}
 
 	return retval;
@@ -162,7 +113,7 @@ unsigned int redis_ping_db (void) {
 	if (redis_context) {
 		redisReply *reply = (redisReply *) redisCommand (redis_context, "PING");
 		if (reply) {
-			fprintf (stdout, "\n[REDIS][PING]: %s\n", reply->str);
+			(void) fprintf (stdout, "\n[REDIS][PING]: %s\n", reply->str);
 			freeReplyObject (reply);
 
 			retval = 0;
@@ -172,62 +123,3 @@ unsigned int redis_ping_db (void) {
 	return retval;
 
 }
-
-#pragma endregion
-
-#pragma region CRUD
-
-// sets a new simple key value
-// returns 0 on success, 1 on error
-unsigned int redis_set_key (const char *key, const char *value) {
-
-	unsigned int retval = 1;
-
-	if (key && value) {
-		redisReply *reply = (redisReply *) redisCommand (
-			redis_context, 
-			"SET %s %s", 
-			key, value
-		);
-
-		if (reply) {
-			printf ("SET: %s\n", reply->str);
-
-			freeReplyObject (reply);
-
-			retval = 0;
-		}
-		
-	}
-
-	return retval;
-
-}
-
-// gets a simple value for a key
-// returns a newly allocated string that should be free
-String *redis_get_value (const char *key) {
-
-	String *retval = NULL;
-
-	if (key) {
-		redisReply *reply = (redisReply *) redisCommand (
-			redis_context, 
-			"SET %s", 
-			key
-		);
-
-		if (reply) {
-			printf ("GET: %s\n", reply->str);
-
-			freeReplyObject (reply);
-
-			retval = str_new (reply->str);
-		}
-	}
-
-	return retval;
-
-}
-
-#pragma endregion
