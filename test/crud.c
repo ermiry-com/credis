cd649a726efff64a59d53b5c3e61f3daf6ed969a
ermiry <erick.salas@ermiry.com>
2021-06-05 19:26:45 -0500

    Added set by type methods unit tests

--- test/crud.c
+++ test/crud.c
@@ -17,6 +17,56 @@ static void credis_test_crud_exists (void) {
 
 }
 
+static void credis_test_crud_set (void) {
+
+	test_check_unsigned_eq (credis_exists ("test"), 1, NULL);
+
+	test_check_unsigned_eq (credis_set ("test", "hola"), 0, NULL);
+
+	test_check_unsigned_eq (credis_exists ("test"), 0, NULL);
+
+}
+
+static void credis_test_crud_set_int (void) {
+
+	test_check_unsigned_eq (credis_exists ("entero"), 1, NULL);
+
+	test_check_unsigned_eq (credis_set_int ("entero", 7), 0, NULL);
+
+	test_check_unsigned_eq (credis_exists ("entero"), 0, NULL);
+
+}
+
+static void credis_test_crud_set_unsigned (void) {
+
+	test_check_unsigned_eq (credis_exists ("unsigned"), 1, NULL);
+
+	test_check_unsigned_eq (credis_set_unsigned ("unsigned", 10), 0, NULL);
+
+	test_check_unsigned_eq (credis_exists ("unsigned"), 0, NULL);
+
+}
+
+static void credis_test_crud_set_large (void) {
+
+	test_check_unsigned_eq (credis_exists ("largo"), 1, NULL);
+
+	test_check_unsigned_eq (credis_set_large ("largo", 1000000), 0, NULL);
+
+	test_check_unsigned_eq (credis_exists ("largo"), 0, NULL);
+
+}
+
+static void credis_test_crud_set_double (void) {
+
+	test_check_unsigned_eq (credis_exists ("doble"), 1, NULL);
+
+	test_check_unsigned_eq (credis_set_double ("doble", 10.1010), 0, NULL);
+
+	test_check_unsigned_eq (credis_exists ("doble"), 0, NULL);
+
+}
+
 int main (int argc, const char **argv) {
 
 	credis_set_hostname ("127.0.0.1");
@@ -25,6 +75,12 @@ int main (int argc, const char **argv) {
 		(void) credis_ping_db ();
 
 		credis_test_crud_exists ();
+
+		credis_test_crud_set ();
+		credis_test_crud_set_int ();
+		credis_test_crud_set_unsigned ();
+		credis_test_crud_set_large ();
+		credis_test_crud_set_double ();
 
 		credis_end ();
 	}
